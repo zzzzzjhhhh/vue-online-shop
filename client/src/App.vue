74cf507c41e1ef69a2ba35ff6c98aefa919c345b
zzj <zhengzijian56@gmail.com>
2020-07-08 23:46:40 +0100

    将 docker 镜像部署到阿里云

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,5 @@
 <template>
   <div id="app">
-    <!-- <nav>
-      <div class="container">
-        <ul class="nav-left">
-          <li>
-            <router-link to="/">Home</router-link>
-          </li>
-          <li>
-            <router-link to="/admin">Admin</router-link>
-          </li>
-          <li>
-            <router-link to="/cart">Cart</router-link>
-          </li>
-        </ul>
-      </div>
-    </nav> -->
     <el-menu
       class="menu"
       default-active="activeIndex"
--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
-var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var index = require("./routes/index");
+var users = require("./routes/users");
 const api = require("./routes/api");
 
 var app = express();
@@ -15,13 +15,18 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// Database connection here
-mongoose.connect(process.env.MONGO_URI || `mongodb://localhost:27017/test`);
+// Datbase connection here
+mongoose.connect(process.env.MONGO_URI || `mongodb://localhost:27017/test`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  user: process.env.MONGO_USER,
+  pass: process.env.MONGO_PASSWORD,
+});
 
 // CORS config here
 app.all("/*", function (req, res, next) {
   // CORS headers
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   // Set custom headers for CORS
   res.header(
@@ -35,19 +40,23 @@ app.all("/*", function (req, res, next) {
   }
 });
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/", index);
+app.use("/users", users);
 app.use("/api/v1", api);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handler
